8a90cadab1f4f129f8a5560d36a2989e7675c460
Saloni Sinha <38814940+saloni027@users.noreply.github.com>
2023-12-19 15:28:38 +0000

    Refacored with useQuery

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,18 +1,28 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/"> Adopt Me! </Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />}></Route>
-        <Route path="/" element={<SearchParams />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/"> Adopt Me! </Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- adopt-me/src/useBreedList.jsx
+++ adopt-me/src/useBreedList.jsx
@@ -1,30 +1,8 @@
-import { useState, useEffect } from "react";
-
-const localCache = new Map();
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache.get(animal)) {
-      setBreedList(localCache.get(animal));
-    } else {
-      requestBreedList();
-    }
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      const res = await fetch(
-        `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache.set(animal, json.breeds || []);
-      setBreedList(json.breeds);
-      setStatus("loaded");
-    }
-  }, [animal]);
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
 }
